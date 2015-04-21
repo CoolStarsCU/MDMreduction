6b41a66637e9a2dcbaba641394cdcb95125444e9
stephtdouglas <stephanietdouglas@gmail.com>
2015-04-21 18:46:43 -0400

    read and sort object list using astropy and use pythonic loops. SO MUCH FASTER

--- fluxcal.py
+++ fluxcal.py
@@ -24,51 +24,37 @@ def fluxcal(imagelist):
 
 # read in a list of spectra as defined in point 1 above:
 
-    input = open(imagelist, 'r')
-    spectra = input.readlines()
-
-# delete first two lines of the table, assuming a line of column headers
-# and a line of whitespace
-   # del spectra[0]
-   # del spectra[0]
-
-# count number of spectra to reduce
-    spectranum = len(spectra)
-
-# go through table line by line and remove spaces, tabs, and end of line marks
-# each cleaned line is turned into a list, where spaces and tabs separate
-# items in a list for each line.  Each line's list is itself nested in a larger
-# list
-
-    i = 0
-    while i < spectranum:
-        spectra[i] = split_strings(spectra[i])
-        i = i + 1
+    image_list = at.read(imagelist,data_start=0,
+                         names=["ccdno","type","target","image_region",
+                                "bias_region","reference_lamp"])
+    spectranum = len(image_list)
         
 # now create important subsets: flats, lamps, biases, objects and std spectra
 
-#first make lists for all the objects
-    sciencelist = []
-    stdlist = []
+# first make lists for all the objects
+    science_list = []
+    science_names = []
+    std_list = []
+    std_names = []
     
-#fill those lists with the names of objects
+# fill those lists with the names of objects
 
-    j = 0
-    while j < spectranum:
-            if spectra[j][1] == 'obj':
-                sciencelist.append(spectra[j])
-                j = j + 1
-            elif spectra[j][1] == 'std':
-                sciencelist.append(spectra[j])
-                stdlist.append(spectra[j])
-                j = j + 1
+    for j,target_type in enumerate(image_list["type"]):
+            if target_type == 'obj':
+                science_list.append(image_list["ccdno"][j])
+                science_names.append(image_list["target"][j])
+            elif target_type == 'std':
+                science_list.append(image_list["ccdno"][j])
+                science_names.append(image_list["target"][j])
+                std_list.append(image_list["ccdno"][j])
+                std_names.append(image_list["target"][j])
             else:
-                j = j + 1
+                continue
 
 # find the number of objects in each list
 
-    numscience = len(sciencelist)
-    numstds = len(stdlist)
+    numscience = len(science_list)
+    numstds = len(std_list)
     
 # now do flux calibration - start with setting airmass to middle of exposure
 
@@ -89,10 +75,8 @@ def fluxcal(imagelist):
     iraf.astutil.setairmass.update = 'yes'
     iraf.astutil.setairmass.override = 'yes'
 
-    c = 0
-    while c < numscience:
-        iraf.astutil.setairmass(images='wavecal/dc.' + sciencelist[c][0])
-        c = c + 1
+    for science_file in science_list:
+        iraf.astutil.setairmass(images='wavecal/dc.' + science_file)
 
 # run standard on our standard star observations
 
@@ -110,11 +94,11 @@ def fluxcal(imagelist):
     iraf.noao.onedspec.standard.graphics = 'stdgraph'
     iraf.noao.onedspec.standard.cursor = ''
 
-    g = 0
-    while g < numstds:
-        iraf.noao.onedspec.standard.star_name = stdlist[g][2]
-        iraf.noao.onedspec.standard(input = 'wavecal/dc.' + stdlist[g][0], output = 'stdfile')
-        g = g + 1
+    for i,std_file in enumerate(std_list):
+        this_std_name = std_names[i].split(".")[0]
+        iraf.noao.onedspec.standard.star_name = this_std_name
+        iraf.noao.onedspec.standard(input = 'wavecal/dc.' + std_file, 
+                                    output = 'stdfile')
     
 # now run sensfunc to get sensitivity functions out
 
@@ -153,15 +137,13 @@ def fluxcal(imagelist):
     # and trimming beyond the good flux calibration region
     os.mkdir('finals')
 
-    aa = 0
-    while aa < numscience:
-        iraf.noao.onedspec.calibrate(input = 'wavecal/dc.' + sciencelist[aa][0],
-                                     output = 'finals/' + sciencelist[aa][2],
+    for j,science_file in enumerate(science_list):
+        iraf.noao.onedspec.calibrate(input = 'wavecal/dc.' + science_file,
+                                     output = 'finals/' + science_names[j],
                                      sensitivity = 'sens')
-        iraf.noao.onedspec.scopy(input='finals/' + sciencelist[aa][2],
-                                 output = 'finals/trim.' + sciencelist[aa][2],
+        iraf.noao.onedspec.scopy(input='finals/' + science_names[j],
+                                 output = 'finals/trim.' + science_names[j],
                                  w1=sensfunc_wave[0],w2=sensfunc_wave[-1],
                                  rebin='no')
-        aa = aa + 1
     
     iraf.flprcache()
